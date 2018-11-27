b8fe070d72c3188ee9e2cfa83e0518e0bb7c0e81
Vladislav Velmisov <velmisovx@gmail.com>
2018-11-27 09:59:13 +0300

    camera light

--- opengl/lab11.cpp
+++ opengl/lab11.cpp
@@ -1,5 +1,7 @@
 #include <GL/freeglut.h>
 #include <math.h>
+#include <iostream>
+using namespace std;
 
 const double PI = acos(-1);
 
@@ -17,8 +19,20 @@ float car_rotate_z = 0.f;
 void init() {
     glClearColor(0, 0, 0, 1);
 
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+
+    glShadeModel(GL_SMOOTH);
+
+    const GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+    const GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+
+    glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.001);
+    glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 60.f);
+
     glEnable(GL_DEPTH_TEST);
-    glMatrixMode(GL_MODELVIEW);
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
 }
 
 // Функция вызывается каждый кадр для его отрисовки
@@ -27,8 +41,17 @@ void update() {
 
     glLoadIdentity();
 
-    glColor3f(1.f, 1.f, 1.f);
-    glRectf(-10.f, -10.f, 10.f, 10.f);
+    glColor3f(1, 1, 1);
+    glBegin(GL_QUADS);
+    glNormal3f(0, 0, 1);
+    glVertex3f(-10, -10, 0);
+    glNormal3f(0, 0, 1);
+    glVertex3f(-10, 10, 0);
+    glNormal3f(0, 0, 1);
+    glVertex3f(10, 10, 0);
+    glNormal3f(0, 0, 1);
+    glVertex3f(10, -10, 0);
+    glEnd();
 
     glTranslatef(car_x, car_y, 0.5f);
     glRotatef(car_rotate_z, 0.f, 0.f, 1.f);
@@ -37,6 +60,37 @@ void update() {
 
     glFlush();
     glutSwapBuffers();
+}
+
+void setCameraLight() {
+    GLint viewport[4];
+    GLdouble modelview_mat[16];
+    GLdouble projection_mat[16];
+    GLdouble camera_posd[3];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    glGetDoublev(GL_MODELVIEW_MATRIX, modelview_mat);
+    glGetDoublev(GL_PROJECTION_MATRIX, projection_mat);
+
+    gluUnProject(double(viewport[2] - viewport[0]) / 2, double(viewport[3] - viewport[1]) / 2, 0.,
+            modelview_mat, projection_mat, viewport,
+            &camera_posd[0], &camera_posd[1], &camera_posd[2]);
+
+    GLfloat camera_pos[4] {0.f, 0.f, 0.f, 1.f};
+    for (int i = 0; i < 3; ++i)
+        camera_pos[i] = (GLfloat)camera_posd[i];
+
+    GLfloat spot_direction[3];
+    for (int i = 0; i < 3; ++i)
+        spot_direction[i] = -camera_pos[i];
+
+    double len = sqrt(spot_direction[0] * spot_direction[0] +
+            spot_direction[1] * spot_direction[1] +
+            spot_direction[2] * spot_direction[2]);
+    for (int i = 0; i < 3; ++i)
+        spot_direction[i] /= len;
+
+    glLightfv(GL_LIGHT0, GL_POSITION, camera_pos);
+    glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
 }
 
 // Обновление камеры
@@ -49,6 +103,9 @@ void updateCamera() {
     glRotatef(camera_rotate_x, 1.f, 0.f, 0.f);
     glRotatef(camera_rotate_z, 0.f, 0.f, 1.f);
     glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    setCameraLight();
 }
 
 // Управление мышкой
@@ -56,6 +113,10 @@ void mouse(int button, int state, int x, int y) {
     if (state == GLUT_DOWN) {
         switch (button) {
             case GLUT_LEFT_BUTTON:
+                if (glIsEnabled(GL_LIGHT0))
+                    glDisable(GL_LIGHT0);
+                else
+                    glEnable(GL_LIGHT0);
                 break;
             case GLUT_RIGHT_BUTTON:
                 break;
@@ -94,6 +155,7 @@ void driving(int key, int x, int y) {
         default:
             return;
     }
+    updateCamera();
     glutPostRedisplay();
 }
 
@@ -102,22 +164,23 @@ void keyboard(unsigned char key, int x, int y) {
     switch (key) {
         case 'a':
             camera_rotate_z += 5;
+            if (camera_rotate_z > 360)
+                car_rotate_z -= 360;
             updateCamera();
             break;
         case 'd':
             camera_rotate_z -= 5;
+            if (camera_rotate_z < -360)
+                camera_rotate_z += 360;
             updateCamera();
             break;
         case 'w':
-            camera_rotate_x += 0.5f;
+            camera_rotate_x += 2;
             updateCamera();
             break;
         case 's':
-            camera_rotate_x -= 0.5f;
+            camera_rotate_x -= 2;
             updateCamera();
-            break;
-        case 'o':
-            glOrtho(-2.f, 2.f, -2.f, 2.f, -2.f, 2.f);
             break;
         default:
             break;
@@ -139,7 +202,6 @@ int main(int argc, char* argv[]) {
     glutInit(&argc, argv);
     glutInitWindowPosition(100, 100);
     glutInitWindowSize(800, 800);
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 
     glutCreateWindow("texture and lightning");
 
